6d1d3d4958a11e2ae44e06b7753e503b9a739836
Tom Benn <tom@fridgecow.com>
2018-02-03 20:34:28 +0000

    Added some framework interfaces for locations. Up for debate!
    Signed-off-by: Tom Benn <tom@fridgecow.com>

---
+++ WearableHouseCoat/app/src/main/java/clquebec/com/framework/location/IndoorLocationProvider.java
@@ -0,0 +1,11 @@
+package clquebec.com.framework.location;
+
+/**
+ * WearableHouseCoat
+ * Author: tom
+ * Creation Date: 03/02/18
+ */
+
+public interface IndoorLocationProvider {
+    Room getRoom();
+}
---
+++ WearableHouseCoat/app/src/main/java/clquebec/com/framework/location/Room.java
@@ -0,0 +1,15 @@
+package clquebec.com.framework.location;
+
+import java.util.UUID;
+
+/**
+ * WearableHouseCoat
+ * Author: tom
+ * Creation Date: 03/02/18
+ */
+
+interface Room {
+    String getName();
+
+    UUID getID();
+}
