6b6ee27631034a241edcb1616ea4b0b45095c6b8
Tom Benn <tom@fridgecow.com>
2018-02-04 08:22:05 +0000

    Added a Person class, with knowledge of its current location.
    Signed-off-by: Tom Benn <tom@fridgecow.com>

---
+++ WearableHouseCoat/app/src/main/java/clquebec/com/framework/people/Person.java
@@ -0,0 +1,52 @@
+package clquebec.com.framework.people;
+
+import android.support.annotation.Nullable;
+
+import java.util.UUID;
+
+import clquebec.com.framework.location.LocationChangeListener;
+import clquebec.com.framework.location.Place;
+
+/**
+ * WearableHouseCoat
+ * Author: tom
+ * Creation Date: 04/02/18
+ */
+
+public class Person {
+    private String mName;
+    private UUID mUUID;
+    private Place mLocation;
+    private LocationChangeListener mListener;
+
+    public Person(UUID id){
+        //TODO: Read information in from somewhere.
+        mUUID = id;
+        mName = "Test Person";
+    }
+
+    public String getName(){
+        return mName;
+    }
+
+    public boolean equals(Object other){
+        return other instanceof Person && ((Person) other).getName().equals(this.getName());
+    }
+
+    public @Nullable Place getLocation(){
+        return mLocation;
+    }
+
+    public void setLocation(Place newLocation){
+        if(!mLocation.equals(newLocation)) {
+            if (mListener != null) {
+                mListener.onLocationChanged(mLocation, newLocation);
+                mLocation = newLocation;
+            }
+        }
+    }
+
+    public void setLocationListener(LocationChangeListener listener){
+        mListener = listener;
+    }
+}
