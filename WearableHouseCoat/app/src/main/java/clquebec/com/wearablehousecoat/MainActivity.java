97a04e745604c8d854ca36c5818fc49f5c4e444b
Tom Benn <tom@fridgecow.com>
2018-02-04 08:37:59 +0000

    Integrated a LocationProvider into MainActivity and increased language level to 8 for that golden lambda syntax
    Signed-off-by: Tom Benn <tom@fridgecow.com>

--- WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/MainActivity.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/MainActivity.java
@@ -9,33 +9,38 @@ import android.widget.TextView;
 
 import clquebec.com.framework.controllable.ControllableDevice;
 import clquebec.com.framework.controllable.ControllableLightDevice;
+import clquebec.com.framework.location.IndoorLocationProvider;
+import clquebec.com.framework.location.LocationChangeListener;
+import clquebec.com.framework.location.Place;
 import clquebec.com.framework.location.Room;
 import clquebec.com.implementations.controllable.IFTTTLight;
+import clquebec.com.implementations.location.DummyLocationProvider;
 import clquebec.com.wearablehousecoat.components.DeviceControlButton;
 
 public class MainActivity extends WearableActivity {
 
     private DeviceControlButton mTestButton;
 
-    private Room mCurrentRoom;
+    private IndoorLocationProvider mLocationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO: Find current room using an IndoorLocationProvider
-        mCurrentRoom = new Room(this, "Test Room");
-
-        //TODO: Automatically generate buttons based on what's in room.
-        //For now, for testing, it seems fine to work without this.
-
         mTestButton = findViewById(R.id.lightButton);
 
-        //TODO: Attach each device to a different button - probably done in previous step.
-        for(ControllableDevice device: mCurrentRoom.getDevices()) {
-            mTestButton.attachDevice(device);
-        }
+        mLocationProvider = new DummyLocationProvider(this);
+        mLocationProvider.setLocationChangeListener((oldLocation, newLocation) -> {
+                //TODO: Automatically generate buttons based on what's in room.
+                //For now, for testing, it seems fine to work without this.
+
+                //TODO: Attach each device to a different button - probably done in previous step.
+                for(ControllableDevice device: newLocation.getDevices()) {
+                    mTestButton.attachDevice(device);
+                }
+            }
+        );
 
         // Enables Always-on
         setAmbientEnabled();
