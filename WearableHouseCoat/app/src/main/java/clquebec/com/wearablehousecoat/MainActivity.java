0878050843ba3adebb5ef8ed8bafcd0985064585
Tom Benn <tom@fridgecow.com>
2018-02-04 08:57:27 +0000

    UI now updates with information about current Place.
    Signed-off-by: Tom Benn <tom@fridgecow.com>

--- WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/MainActivity.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/MainActivity.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Set;
+
 import clquebec.com.framework.controllable.ControllableDevice;
 import clquebec.com.framework.controllable.ControllableLightDevice;
 import clquebec.com.framework.location.IndoorLocationProvider;
 import clquebec.com.framework.location.LocationChangeListener;
 import clquebec.com.framework.location.Place;
 import clquebec.com.framework.location.Room;
+import clquebec.com.framework.people.Person;
 import clquebec.com.implementations.controllable.IFTTTLight;
 import clquebec.com.implementations.location.DummyLocationProvider;
 import clquebec.com.wearablehousecoat.components.DeviceControlButton;
@@ -20,6 +23,8 @@ import clquebec.com.wearablehousecoat.components.DeviceControlButton;
 public class MainActivity extends WearableActivity {
 
     private DeviceControlButton mTestButton;
+    private TextView mLocationNameView;
+    private TextView mPersonCountView;
 
     private IndoorLocationProvider mLocationProvider;
 
@@ -28,10 +33,22 @@ public class MainActivity extends WearableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationNameView = findViewById(R.id.main_currentlocation);
+        mPersonCountView = findViewById(R.id.main_companions);
         mTestButton = findViewById(R.id.lightButton);
 
         mLocationProvider = new DummyLocationProvider(this);
         mLocationProvider.setLocationChangeListener((oldLocation, newLocation) -> {
+                mLocationNameView.setText(newLocation.getName());
+
+                Set<Person> people = newLocation.getPeople();
+                mPersonCountView.setText(
+                        getResources().getQuantityString( //Automatically varies based on number
+                                R.plurals.companion_strings,
+                                people.size(),
+                                people.size()
+                        ));
+
                 //TODO: Automatically generate buttons based on what's in room.
                 //For now, for testing, it seems fine to work without this.
 
