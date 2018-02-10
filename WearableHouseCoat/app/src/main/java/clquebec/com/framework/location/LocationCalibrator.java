8f2aaed06015dfef9903b99b7a1e87d391dbc841
Tom Benn <tom@fridgecow.com>
2018-02-10 00:09:42 +0000

    Added a calibrate method which takes JSONObject data.
    Signed-off-by: Tom Benn <tom@fridgecow.com>

--- WearableHouseCoat/app/src/main/java/clquebec/com/framework/location/LocationCalibrator.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/framework/location/LocationCalibrator.java
@@ -1,5 +1,7 @@
 package clquebec.com.framework.location;
 
+import org.json.JSONObject;
+
 /**
  * Created by reeto on 09/02/18.
  */
@@ -9,8 +11,13 @@ public interface LocationCalibrator {
 
     // Add a room to fingerprint binding to the system
     boolean calibrate(Room room);
+
     // String is to encode arbitrary data - Uses knowledge that underlying system is a server
     boolean calibrate(Room room, String data);
+
+    //Like above, but encodes arbitrary JSON data
+    boolean calibrate(Room room, JSONObject data);
+
     // Remove all data about Room to fingerprint bindings from the system
     boolean reset();
 }
