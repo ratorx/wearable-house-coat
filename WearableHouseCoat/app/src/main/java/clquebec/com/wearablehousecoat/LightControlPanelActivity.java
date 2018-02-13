cd3873cfde10c99a7791dc8257904c42a3f9d416
MasterObvious <me@timlazarus.com>
2018-02-13 17:46:18 +0000

    Added colour picker to light control panel

--- WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/LightControlPanelActivity.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/LightControlPanelActivity.java
@@ -1,12 +1,24 @@
 package clquebec.com.wearablehousecoat;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.jraf.android.androidwearcolorpicker.app.ColorPickActivity;
+
 public class LightControlPanelActivity extends WearableActivity {
+    private final static int REQUEST_PICK_COLOR = 1;
 
     private TextView mTextView;
+    private ImageView mColourPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,32 @@ public class LightControlPanelActivity extends WearableActivity {
 
         // Enables Always-on
         setAmbientEnabled();
+
+        mColourPreview = findViewById(R.id.colourPreview);
+        mColourPreview.setTag(Color.valueOf(0xffffffff));
+        mColourPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new ColorPickActivity.IntentBuilder().oldColor(((Color) mColourPreview.getTag()).toArgb()).build(LightControlPanelActivity.this);
+                startActivityForResult(intent, REQUEST_PICK_COLOR);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_PICK_COLOR:
+                if (resultCode == RESULT_CANCELED) {
+                    // The user pressed 'Cancel'
+                    break;
+                }
+
+                int pickedColor = ColorPickActivity.getPickedColor(data);
+                mColourPreview.setTag(Color.valueOf(pickedColor));
+                mColourPreview.setColorFilter(pickedColor);
+                break;
+        }
     }
 }
