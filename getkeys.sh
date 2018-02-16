ab1b9a87f48971712a3e51031dd1060790361fad
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-02-16 21:37:19 +0000

    Add script to get API keys

---
+++ getkeys.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -e ".git" ]; then
+    echo "Error: Should be run from base git directory"
+    exit 1
+fi
+
+rm -rf 'WearableHouseCoat/app/src/main/java/clquebec/com/environment'
+
+crsid=""
+if [ $# -ne 1 ]; then
+    echo -n "crsid: "
+    read crsid
+else
+    crsid=$1
+fi
+
+scp -r "$crsid@shell.srcf.net:clquebec/environment" WearableHouseCoat/app/src/main/java/clquebec/com/environment
+find WearableHouseCoat/app/src/main/java/clquebec/com/environment/* -type f -exec git update-index --assume-unchanged '{}' 2>/dev/null \;
