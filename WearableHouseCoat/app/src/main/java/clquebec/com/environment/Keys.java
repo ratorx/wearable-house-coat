2b820c9316d081d78667c7e4bcfba51545b30fa1
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2018-02-17 19:23:18 +0000

    Update Keys file

---
+++ WearableHouseCoat/app/src/main/java/clquebec/com/environment/Keys.java
@@ -0,0 +1,10 @@
+package clquebec.com.environment;
+
+/**
+ * Created by reeto on 06/02/18.
+ */
+
+public final class Keys {
+    public static final String IFTTT = "";
+	public static final String ConfigJSON = "";
+}
