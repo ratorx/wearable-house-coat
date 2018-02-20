27bb7d344ac6f56b0a2b76b6e82f2fde2ebdefb7
zlotnleo <zlotnleo@gmail.com>
2018-02-20 12:59:01 +0000

    Dynamically changing the page based on the navbar

--- web/src/App/App.js
+++ web/src/App/App.js
@@ -1,32 +1,49 @@
-"use strict"
-
 import React from 'react';
 import './App.css';
-import Lorem from './Lorem.js';
+import Overview from './components/Overview.js'
+import SetRooms from './components/SetRooms.js'
+import SetDevices from './components/SetDevices.js'
+import SetGroups from './components/SetGroups.js'
+import Help from './components/Help.js'
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			Pages: [
-				{name: "Overview"},
+			pages: [
+				{
+					name: "Overview",
+					component: <Overview/>
+				},
 				{
 					name: "Setup",
 					dropdown: [
-						{name: "Rooms"},
-						{name: "Devices"},
-						{name: "Groups"}
+						{
+							name: "Rooms",
+							component: <SetRooms/>
+						},
+						{
+							name: "Devices",
+							component: <SetDevices/>
+						},
+						{
+							name: "Groups",
+							component: <SetGroups/>
+						}
 					]
 				},
-				{name: "Help"}
-			],
-			currentPage: "Overview"
+				{
+					name: "Help",
+					component: <Help/>
+				}
+			]
 		}
+		this.state.currentPage = this.state.pages[0]
 	}
 
 	setCurrentPage(page) {
-		this.setState(() => {return {currentPage: page.name}})
+		this.setState(() => {return {currentPage: page}})
 	}
 
 	render() {
@@ -41,14 +58,14 @@ class App extends React.Component {
 				<Navbar.Collapse>
 					<Nav>
 						{
-							this.state.Pages.map((page, i) => {
+							this.state.pages.map((page, i) => {
 								if(typeof page.dropdown === "undefined") {
-									return <NavItem key={i} active={page.name === this.state.currentPage} onClick={() => {this.setCurrentPage(page)}}>{page.name}</NavItem>
+									return <NavItem key={i} active={page.name === this.state.currentPage.name} onClick={() => {this.setCurrentPage(page)}}>{page.name}</NavItem>
 								} else {
 									return <NavDropdown key={i} title={page.name} id={"main-navbar-dropdown-" + page.name}>
 										{
 											page.dropdown.map((subpage, j) =>
-												<MenuItem key={i + "." + j} active={subpage.name === this.state.currentPage} onClick={() => {this.setCurrentPage(subpage)}}>{subpage.name}</MenuItem>
+												<MenuItem key={i + "." + j} active={subpage.name === this.state.currentPage.name} onClick={() => {this.setCurrentPage(subpage)}}>{subpage.name}</MenuItem>
 											)
 										}
 									</NavDropdown>
@@ -58,7 +75,9 @@ class App extends React.Component {
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
-			<Lorem/>
+			{
+				this.state.currentPage.component
+			}
 		</div>
 	}
 }
