cbfd3efe002898a8761dc4df2ca6f6915d2a9c71
Tom Benn <tom@fridgecow.com>
2018-02-21 16:32:26 +0000

    Implemented a test for instantiating Room from JSON
    Signed-off-by: Tom Benn <tom@fridgecow.com>

---
+++ WearableHouseCoat/app/src/main/java/clquebec/com/implementations/controllable/DummyControllable.java
@@ -0,0 +1,71 @@
+package clquebec.com.implementations.controllable;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import clquebec.com.framework.controllable.ControllableDevice;
+import clquebec.com.framework.controllable.ControllableDeviceType;
+
+/**
+ * WearableHouseCoat
+ * Author: tom
+ * Creation Date: 21/02/18
+ */
+
+public class DummyControllable implements ControllableDevice {
+    //This class is for testing. It contains no implementations.
+    //Since Controllable classes are loaded dynamically, this is required.
+    //Tests will probably fail if you remove this.
+
+    private String mLocation;
+
+    public DummyControllable(Context c, JSONObject config) throws JSONException{
+        mLocation = config.getString("location");
+    }
+
+    public String getLocation(){
+        return mLocation;
+    }
+
+    @Override
+    public boolean enable() {
+        return true;
+    }
+
+    @Override
+    public boolean disable() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public ControllableDeviceType getType() {
+        return ControllableDeviceType.LIGHT;
+    }
+
+    @Override
+    public boolean quickAction() {
+        return false;
+    }
+
+    @Override
+    public boolean extendedAction() {
+        return false;
+    }
+}
--- WearableHouseCoat/app/src/test/java/RoomTest.java
+++ WearableHouseCoat/app/src/test/java/RoomTest.java
@@ -2,11 +2,21 @@ import android.content.Context;
 
 import junit.framework.TestCase;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.UUID;
+
+import clquebec.com.framework.controllable.ControllableDevice;
 import clquebec.com.framework.location.Room;
+import clquebec.com.implementations.controllable.DummyControllable;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 /**
  * WearableHouseCoat
@@ -14,21 +24,25 @@ import static org.mockito.Mockito.mock;
  * Creation Date: 19/02/18
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class RoomTest extends TestCase {
     //Test the methods of Room
+
+    @Mock
+    private Context mContext;
 
     private String mTestName = "TestRoom";
     private Room mRoom;
 
+    @Test
     public void testEquals(){
         EqualsVerifier.forClass(Room.class);
     }
 
+    @Test
     public void testGetName(){
-        Context c = mock(Context.class);
-
         //Instantiation should set name
-        mRoom = new Room(c, mTestName);
+        mRoom = new Room(mContext, mTestName);
         assertThat(mRoom.getName()).isEqualTo(mTestName);
 
         //Set name should set name
@@ -37,17 +51,28 @@ public class RoomTest extends TestCase {
         assertThat(mRoom.getName()).isEqualTo(newName);
     }
 
-    public void testGetPeople(){
-        //TODO: When Room has more than an example implementation, do this.
+    @Test
+    public void testGetDevices() throws JSONException {
+        //Instantiate a room from some JSON
+        mRoom = new Room(mContext, new JSONObject(
+                "{'name':'testroom', 'id': 1, 'devices':[{'type': 'DummyControllable', 'config': {}}]}"
+        ));
+
+        //Check that all was instantiated correctly
+        assertThat(mRoom.getID()).isEqualTo(new UUID(0, 1));
+        assertThat(mRoom.getName()).isEqualTo("testroom");
+        assertThat(mRoom.getDevices().size()).isEqualTo(1);
+
+        ControllableDevice d = mRoom.getDevices().get(0);
+        assertThat(d).isInstanceOf(DummyControllable.class);
+
+        //Check that the "location" string was passed through by Room correctly.
+        assertThat(((DummyControllable) d).getLocation()).isEqualTo("testroom");
     }
 
-    public void testGetDevices(){
-        //TODO: When Room has more than an example implementation, do this.
-    }
-
+    @Test
     public void testGetType(){
-        Context c = mock(Context.class);
-        mRoom = new Room(c, mTestName);
+        mRoom = new Room(mContext, mTestName);
 
         //Type should be null - if a Room type is ever added, change this test.
         assertThat(mRoom.getType()).isNull();
