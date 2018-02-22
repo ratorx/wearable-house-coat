70ddcf0a4aea3e613b13236276ba262bbe40a35a
MasterObvious <me@timlazarus.com>
2018-02-22 14:44:47 +0000

    Add color picker integration with Hue
    This is still experimental code but less so. Feel free to tidy up and
    add to.

--- WearableHouseCoat/app/src/main/java/clquebec/com/environment/Keys.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/environment/Keys.java
@@ -5,5 +5,36 @@ package clquebec.com.environment;
  */
 
 public final class Keys {
-    public static final String IFTTT = "";
+    public static final String IFTTT = "d5xeD19a2M8W439R_YD4Bf";
+
+		public static final String ConfigJSON = "{"+
+			"'server': 'http://shell.srcf.net:8003/',"+
+			"'name': 'tom',"+
+			"'uid': 'aaaa-bbbb-cccc',"+
+			"'rooms': [{"+
+				"'name':'Intel Lab',"+
+				"'uid': 'xxxxx-yyyy-zzzzz',"+
+				"'devices': [{"+
+					"'type': 'IFTTTLight',"+
+					"'config': {"+
+						"'name': 'Intel Light'"+
+					"}"+
+			"}, {"+
+				"'name':'The Street',"+
+				"'uid':'aaaa-bbbb-cccc-d',"+
+				"'devices':[{"+
+					"'type': 'PhillipsHue',"+
+					"'config': {"+
+						"'name': 'Hue Bulb 1'"+
+					"}"+
+			"}, {"+
+				"'name':'Cafe',"+
+				"'uid':'aaaaaaaaaa',"+
+				"'devices':[{"+
+					"'type': 'Chromecast',"+
+					"'config': {"+
+						"'name': 'Chromecast 1'"+
+					"}"+
+			"}"+
+		"}";
 }
--- WearableHouseCoat/app/src/main/java/clquebec/com/implementations/controllable/PhilipsHue.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/implementations/controllable/PhilipsHue.java
@@ -1,15 +1,21 @@
 package clquebec.com.implementations.controllable;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Parcelable;
 import android.util.Log;
 
 import com.philips.lighting.hue.sdk.wrapper.connection.BridgeConnection;
 import com.philips.lighting.hue.sdk.wrapper.connection.BridgeConnectionCallback;
 import com.philips.lighting.hue.sdk.wrapper.connection.BridgeConnectionType;
 import com.philips.lighting.hue.sdk.wrapper.connection.BridgeResponseCallback;
+import com.philips.lighting.hue.sdk.wrapper.connection.BridgeStateCacheType;
 import com.philips.lighting.hue.sdk.wrapper.connection.BridgeStateUpdatedCallback;
 import com.philips.lighting.hue.sdk.wrapper.connection.BridgeStateUpdatedEvent;
 import com.philips.lighting.hue.sdk.wrapper.connection.ConnectionEvent;
+import com.philips.lighting.hue.sdk.wrapper.connection.HeartbeatManager;
+import com.philips.lighting.hue.sdk.wrapper.connection.LocalBridgeConnection;
 import com.philips.lighting.hue.sdk.wrapper.domain.Bridge;
 import com.philips.lighting.hue.sdk.wrapper.domain.BridgeBuilder;
 import com.philips.lighting.hue.sdk.wrapper.domain.BridgeState;
@@ -25,6 +31,7 @@ import java.util.List;
 import clquebec.com.framework.controllable.ActionNotSupported;
 import clquebec.com.framework.controllable.ControllableDeviceType;
 import clquebec.com.framework.controllable.ControllableLightDevice;
+import clquebec.com.wearablehousecoat.LightControlPanelActivity;
 
 
 /**
@@ -37,11 +44,19 @@ public class PhilipsHue implements ControllableLightDevice {
     //TODO: Implement this.
     private static Bridge bridge = null;
     private boolean enabled = true;
+    private Context mContext;
+    private HeartbeatManager mHbm;
+
 
     private BridgeConnectionCallback bridgeConnectionCallback = new BridgeConnectionCallback() {
         @Override
         public void onConnectionEvent(BridgeConnection bridgeConnection, ConnectionEvent connectionEvent) {
             Log.d("Hue", "Connection event: " + connectionEvent);
+            if (connectionEvent.equals("AUTHENTICATED")) {
+                HeartbeatManager hbm = bridgeConnection.getHeartbeatManager();
+                hbm.startHeartbeat(BridgeStateCacheType.LIGHTS_AND_GROUPS, 100);
+                mHbm = hbm;
+            }
         }
 
         public void onConnectionError(BridgeConnection bridgeConnection, List<HueError> hueErrors){
@@ -64,29 +79,29 @@ public class PhilipsHue implements ControllableLightDevice {
         //Pick the right one(s)
 
         //Initialise internal state.
+        mContext = c;
         //connect to bridge. This is currently hardcoded. Need to implement discovery
         if (bridge == null){
             bridge = new BridgeBuilder("Wearable House Control", "CLWatch")
-                    .setIpAddress("192.168.14.136")
+                    .setIpAddress("192.168.14.220")
                     .setConnectionType(BridgeConnectionType.LOCAL)
                     .setBridgeConnectionCallback(bridgeConnectionCallback)
                     .addBridgeStateUpdatedCallback(bridgeStateUpdatedCallback)
                     .build();
 
             Log.d("Hue", "Connected to bridge");
-            bridge.connect();
-            Log.d("Hue", bridge.toString());
+            BridgeConnection connection = bridge.getBridgeConnection(BridgeConnectionType.LOCAL);
+            connection.connect();
+
+
+
+
         }
 
     }
 
     @Override
     public void setLightColor(int color) throws ActionNotSupported {
-
-    }
-
-    @Override
-    public boolean enable() {
         Log.d("Hue", "Running this");
         BridgeState bs = bridge.getBridgeState();
 
@@ -97,11 +112,13 @@ public class PhilipsHue implements ControllableLightDevice {
             final LightState lightState = new LightState();
 
             //lightState.setOn(true);
-            HueColor hc = new HueColor(new HueColor.RGB(255,0,217),
+            int r = Color.red(color);
+            int g = Color.green(color);
+            int b = Color.blue(color);
+            HueColor hc = new HueColor(new HueColor.RGB(r,g,b),
                     light.getLightConfiguration().getModelIdentifier(),
                     light.getLightConfiguration().getSwVersion());
             lightState.setXY(hc.getXY().x,hc.getXY().y);
-            Log.d("Hue", "sent turn on request");
 
             light.updateState(lightState, BridgeConnectionType.LOCAL, new BridgeResponseCallback() {
                 @Override
@@ -116,10 +133,36 @@ public class PhilipsHue implements ControllableLightDevice {
                     }
                 }
             });
-
         }
-        enabled = true;
-        return true;
+    }
+
+    public int getColor() {
+            BridgeState bs = bridge.getBridgeState();
+            bs.refresh(BridgeStateCacheType.FULL_CONFIG, BridgeConnectionType.LOCAL);
+            List<LightPoint> lights = bs.getLights();
+
+            if (lights.size() > 0){
+               LightPoint testLight = lights.get(2);
+                HueColor.RGB rgb = testLight.getLightState().getColor().getRGB();
+
+                return getIntFromColor(rgb.r, rgb.g, rgb.b);
+            }
+
+        return 20;
+    }
+
+    private int getIntFromColor(int Red, int Green, int Blue){
+        Red = (Red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
+        Green = (Green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
+        Blue = Blue & 0x000000FF; //Mask out anything not blue.
+
+        return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+    }
+
+
+    @Override
+    public boolean enable() {
+        return false;
     }
 
 
@@ -185,6 +228,10 @@ public class PhilipsHue implements ControllableLightDevice {
 
     @Override
     public boolean extendedAction() {
-        return false;
+        Intent lightControls = new Intent(mContext, LightControlPanelActivity.class);
+        lightControls.putExtra("DeviceType", "HueLight");
+        mContext.startActivity(lightControls);
+
+        return true;
     }
 }
