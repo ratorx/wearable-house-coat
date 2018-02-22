1580ed46cdde2d23e65ef6ada31f9491bb2d907a
MasterObvious <me@timlazarus.com>
2018-02-22 14:44:47 +0000

    Add color picker integration with Hue
    This is still experimental code but less so. Feel free to tidy up and
    add to.

--- WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/LightControlPanelActivity.java
+++ WearableHouseCoat/app/src/main/java/clquebec/com/wearablehousecoat/LightControlPanelActivity.java
@@ -4,16 +4,26 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.jraf.android.androidwearcolorpicker.app.ColorPickActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import clquebec.com.framework.controllable.ActionNotSupported;
+import clquebec.com.implementations.controllable.PhilipsHue;
 
 public class LightControlPanelActivity extends WearableActivity {
     private final static int REQUEST_PICK_COLOR = 1;
 
     private TextView mTextView;
     private ImageView mColourPreview;
+    //This is temporary code to test the control of light colour and brightness
+    private static PhilipsHue phTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +34,21 @@ public class LightControlPanelActivity extends WearableActivity {
         setAmbientEnabled();
 
         mColourPreview = findViewById(R.id.colourPreview);
-        mColourPreview.setTag(Color.WHITE);
         mColourPreview.setOnClickListener(view -> {
             Intent intent = new ColorPickActivity.IntentBuilder().oldColor(((Integer) mColourPreview.getTag())).build(LightControlPanelActivity.this);
             startActivityForResult(intent, REQUEST_PICK_COLOR);
         });
+
+        if (phTest == null){
+            phTest = new PhilipsHue(this);
+        }
+
+        String dT = getIntent().getExtras().getString("DeviceType");
+        if (dT != null && dT.equals("HueLight")){
+            mColourPreview.setColorFilter(phTest.getColor());
+            mColourPreview.setTag(phTest.getColor());
+        }
+
     }
 
     @Override
@@ -40,10 +60,27 @@ public class LightControlPanelActivity extends WearableActivity {
                     // The user pressed 'Cancel'
                     break;
                 }
-
                 int pickedColor = ColorPickActivity.getPickedColor(data);
-                mColourPreview.setTag(pickedColor);
-                mColourPreview.setColorFilter(pickedColor);
+                String dT = getIntent().getExtras().getString("DeviceType");
+                if (dT != null && dT.equals("HueLight")){
+                    try{
+                        phTest.setLightColor(pickedColor);
+
+                        Timer mHereTimer = new Timer();
+                        mHereTimer.schedule(new TimerTask() {
+                            public void run() {
+                                runOnUiThread(() -> {int c = phTest.getColor();
+                                Log.d("Hue", "Set color is " + pickedColor + ", get color is " + c);
+                                mColourPreview.setColorFilter(c);
+                                mColourPreview.setTag(c);});
+                            }
+                        }, 3000);
+
+                    }catch (ActionNotSupported e){
+                        Log.e("LightControl", "SHOULD NEVER GET HERE");
+                        assert(false);
+                    }
+                }
                 break;
         }
     }
