4f7ec3e1c3912d61cc0b4c99980c09200e80541e
jack <jwd44@cam.ac.uk>
2018-02-26 15:00:54 +0000

    First full version of playback control panel. Changes to come after discussion with team

--- WearableHouseCoat/app/src/main/java/com/clquebec/wearablehousecoat/MediaControlPanelActivity.java
+++ WearableHouseCoat/app/src/main/java/com/clquebec/wearablehousecoat/MediaControlPanelActivity.java
@@ -16,6 +16,8 @@ import com.clquebec.framework.listenable.DeviceChangeListener;
 import com.clquebec.framework.listenable.ListenableDevice;
 import com.clquebec.framework.storage.ConfigurationStore;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 
 public class MediaControlPanelActivity extends WearableActivity implements DeviceChangeListener {
@@ -29,12 +31,13 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
     private ImageView mPreviousButton;
     private ImageView mPlayPauseButton;
     private ImageView mNextButton;
+    private ImageView mVolumeIcon;
     private ControllablePlaybackDevice mPlaybackDevice;
     private boolean changingVolume = false;
     private boolean changingBrightness = false;
     private boolean currentlyPlaying = false;
-    private boolean hasNext;
-    private boolean hasPrevious;
+
+    private static final int barMax = 255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -44,28 +47,64 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
         setAmbientEnabled();
 
         // Binding UI elements to useful variables
+        mVolumeIcon = findViewById(R.id.volumeIcon);
+        mVolumeWrapper = findViewById(R.id.volumeControlLayout);
+        mBrightnessWrapper = findViewById(R.id.brightnessControlLayout);
 
         // Volume slider
         mVolumeBar = findViewById(R.id.volumeBar);
-        mVolumeBar.setMax(255);
+        mVolumeBar.setMax(barMax);
         mVolumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {}
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                // If this doesn't work on a listener update, copy this into the listener
+                // and introduce a check here for user input (on boolean b)
+                double prog = seekBar.getProgress() / barMax;
+                if(prog == 0){
+                    mVolumeIcon.setImageResource(R.drawable.ic_volume_off);
+                }
+                // TODO:tweak these values for prettiness
+                else if(prog < 0.3){
+                    mVolumeIcon.setImageResource(R.drawable.ic_volume_low);
+                }
+                else if(prog < 0.8){
+                    mVolumeIcon.setImageResource(R.drawable.ic_volume_med);
+                }
+                else{
+                    mVolumeIcon.setImageResource(R.drawable.ic_volume_high);
+                }
+            }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                changingVolume = true;
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                Timer volumeTimer = new Timer();
+                volumeTimer.schedule(new TimerTask() {
+                    public void run() {
+                        runOnUiThread(() -> {
+                            changingVolume = false;
+                            try{
+                                mVolumeBar.setProgress(mPlaybackDevice.getVolume());
+                            }catch (ActionNotSupported e) {
+                                Log.e(TAG, "Device does not support getting brightness");
+                            }});
+                    }
+                }, 500);
+                try {
+                    mPlaybackDevice.setVolume(seekBar.getProgress());
+                }catch(ActionNotSupported e){
+                    Log.e(TAG, "Device does not support setting brightness");
+                }
             }
         });
 
         // Brightness slider
         mBrightnessBar = findViewById(R.id.brightnessBar);
-        mBrightnessBar.setMax(255);
+        mBrightnessBar.setMax(barMax);
         mBrightnessBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -74,12 +113,28 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                changingBrightness = true;
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                Timer brightnessTimer = new Timer();
+                brightnessTimer.schedule(new TimerTask() {
+                    public void run() {
+                        runOnUiThread(() -> {
+                            changingBrightness = false;
+                            try{
+                                mBrightnessBar.setProgress(mPlaybackDevice.getBrightness());
+                            }catch (ActionNotSupported e) {
+                                Log.e(TAG, "Device does not support getting brightness");
+                            }});
+                    }
+                }, 500);
+                try {
+                    mPlaybackDevice.setBrightness(seekBar.getProgress());
+                }catch(ActionNotSupported e){
+                    Log.e(TAG, "Device does not support setting brightness");
+                }
             }
         });
 
@@ -90,7 +145,8 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
             if(currentlyPlaying){
                 mPlayPauseButton.setImageResource(R.drawable.ic_pause_button);
             }
-        }catch(ActionNotSupported ans){
+        }
+        catch(ActionNotSupported ans){
             Log.e(TAG, "PB device does not support playing status check");
         }
         mPlayPauseButton.setOnClickListener((view) -> {
@@ -114,10 +170,10 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
             }
         });
 
-        /* TODO:
-                Add functionality for enabling/disabling buttons based on availability
-                Means updating when a new item is added to the queue (could be done by
-                another user or device)
+        /* TODO: Disable the buttons when unusable (use the "disabled" icon resources)
+                 Add functionality for enabling/disabling buttons based on availability
+                 Means updating when a new item is added to the queue (could be done by
+                 another user or device)
         */
         // Previous button
         mPreviousButton = findViewById(R.id.mediaLeft);
@@ -141,6 +197,7 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
             }
         });
 
+
         if(getIntent().getExtras() == null){
             throw new IllegalArgumentException("LightControlPanelActivity must be given a Device ID");
         }
@@ -162,6 +219,8 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
             }
 
             // A messy way of checking if we have volume and brightness enabled.
+            // TODO: Make this clearer. We only want to hide the bars if we can't SET the values
+            // TODO: (Although it'd be odd to be able to get and not set).
             try {
                 mVolumeBar.setProgress(mPlaybackDevice.getVolume());
             }catch(ActionNotSupported e){
@@ -177,8 +236,6 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
             }
         });
 
-
-
     }
 
     @Override
@@ -192,6 +249,22 @@ public class MediaControlPanelActivity extends WearableActivity implements Devic
 
     @Override
     public void updateState(ListenableDevice device) {
-
+        ControllablePlaybackDevice playbackDevice = (ControllablePlaybackDevice) device;
+        if(!changingBrightness){
+            try{
+                mBrightnessBar.setProgress(playbackDevice.getBrightness());
+            }
+            catch(ActionNotSupported ans){
+                Log.e(TAG, "Device does not support brightness");
+            }
+        }
+        if(!changingVolume){
+            try{
+                mVolumeBar.setProgress(playbackDevice.getVolume());
+            }
+            catch(ActionNotSupported ans){
+                Log.e(TAG, "Device does not support volume");
+            }
+        }
     }
 }
