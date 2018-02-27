46fd9a53efb06486aede274f61800dcd9f6d0fac
MasterObvious <me@timlazarus.com>
2018-02-27 13:19:22 +0000

    Change Playback interface
    Also added space for art

--- WearableHouseCoat/app/src/main/java/com/clquebec/framework/controllable/ControllablePlaybackDevice.java
+++ WearableHouseCoat/app/src/main/java/com/clquebec/framework/controllable/ControllablePlaybackDevice.java
@@ -10,27 +10,20 @@ package com.clquebec.framework.controllable;
 
 public interface ControllablePlaybackDevice extends ControllableDevice {
 
-    //Sets the currently playing resource
-    void setResource(String resource);
-
     //Gets the currently playing resource
     String getResource();
 
     // Returns true if there's another item queued for playing (so the skip button can
     // do something worthwhile).
-    boolean getNext() throws ActionNotSupported;
 
     // Skips to the next item (song/video)
     boolean skipNext() throws ActionNotSupported;
-
-    // Returns true if there's a previous item to return to.
-    boolean getPrevious() throws ActionNotSupported;
 
     // Returns to the previous item
     boolean skipPrevious() throws ActionNotSupported;
 
     // Pauses/resumes playback
-    boolean setPlaying(boolean enabled);
+    boolean togglePlaying();
 
     //Gets whether the device is currently playing
     boolean getPlaying() throws ActionNotSupported;
@@ -41,9 +34,6 @@ public interface ControllablePlaybackDevice extends ControllableDevice {
     //Gets the volume
     int getVolume() throws ActionNotSupported;
 
-    //Sets the brightness
-    boolean setBrightness(float brightness) throws ActionNotSupported;
+    String getArtLocation();
 
-    //Gets the brightness
-    int getBrightness() throws ActionNotSupported;
 }
