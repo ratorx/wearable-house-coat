613ec15c12bac498627cd0a4b6342ac3c0b56b8e
zlotnleo <zlotnleo@gmail.com>
2018-02-27 20:43:20 +0000

    Added random UUID generation

--- web/src/App/util/DeviceInfo.js
+++ web/src/App/util/DeviceInfo.js
@@ -58,7 +58,8 @@ class DeviceInfo {
 	}
 
 	createUid() {
-		return "dkfjahgsdkjfhaslkdjfhalskjdhflaksjdhflajsdhflkajh";
+		let s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 	}
 }
 
