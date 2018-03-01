9d15b61c7e643afe054e1048482ef6760416bee5
MasterObvious <me@timlazarus.com>
2018-03-01 13:45:21 +0000

    Sort out git issues

---
+++ web/src/App/components/Automations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PageHeader } from 'react-bootstrap';
+import Lorem from './Lorem.js';
+
+const Automations = (props) =>
+	<div>
+		<PageHeader>Automations</PageHeader>
+		<Lorem/>
+	</div>
+
+export default Automations
