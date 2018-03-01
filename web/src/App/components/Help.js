b19661e98940ef9b4825a3fe592a12e877b96d94
jack <jwd44@cam.ac.uk>
2018-03-01 15:41:49 +0000

    More help stuff

--- web/src/App/components/Help.js
+++ web/src/App/components/Help.js
@@ -18,9 +18,19 @@ const Help = (props) => {
 		<h4>{"\"How do I change the colour of my lights?\""}</h4>
 		<p>Press and hold the button of the light (or group of lights) you want to change. This will bring up the control panel; you can then press the big preview circle to display the colour selector</p>
 		<br/>
+		<h4>{"\"I tried pressing the skip button on my music, but it's greyed out and doesn't do anything.\""}</h4>
+		<p>This means that the skip has been disabled, most likely because it isn{"\'"}t supported by your device.</p>
+		<br/>
+		<h4>{"\"I\'m using a router to extend my work/uni network, but nothing is working.\""}</h4>
+		<p>Some networks <em>really</em> {"don\'t"} like to be extended. You will need to find an alternative connection.</p>
+		<br/>
+		<h4>{"\"I have an excellent idea for the project.\""}</h4>
+		<p>Let us know by emailing us at <a href="mailto:clquebec-admins@srcf.net?subject=Wearable%20House%20%Coat idea!">clquebec-admins@srcf.net</a></p>
+		<br/>
 		<h4>{"\"I hit the ship, but they used a decoy\""}</h4>
 		<p>Try using some weird poisonous space centipedes, that should work.</p>
 		<br/>
 	</div>
 }
+
 export default Help
