de216049db45fe621783c3bccd164ea153af1f4a
MasterObvious <me@timlazarus.com>
2018-03-04 16:41:21 +0000

    Include dummy strings for Spotify client authentication

--- WearableHouseCoat/app/src/main/java/com/clquebec/environment/Keys.java
+++ WearableHouseCoat/app/src/main/java/com/clquebec/environment/Keys.java
@@ -7,5 +7,8 @@ package com.clquebec.environment;
 public final class Keys {
     public static final String IFTTT = "";
 
-		public static final String ConfigJSON = "";
+	public static final String ConfigJSON = "";
+
+	public static final String SPOTIFY_CLIENTID = "";
+	public static final String SPOTIFY_CLIENT_SECRET="";
 }
